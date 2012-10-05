7150b3a1784413b44559d9d6c8a1b5a4b0efaa50
mottersheadt <travis@mottershead.biz>
2012-10-05 16:10:47 -0600

    added the bootstrap library to the repo, as well as a basic page 'default.php'

---
+++ website/default.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" >
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.min.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"
+              type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/add-ons.css" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <a class="btn btn-navbar" data-toggle="collapse"
+                   data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a class="brand">Be The Critic</a>
+                <div class="nav-collapse">
+                    <ul class="nav">
+                        <li class="active"><a>Home</a></li>
+                        <li><a>How It Works</a></li>
+                        <li><a>Add Your Own</a></li>
+                        <li><a>Your Critiques</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+            <div class="container">
+                
+                <h1>You Be The Critic!</h1>
+                <p>please.</p>
+            </div>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+    </body>
+</html>
