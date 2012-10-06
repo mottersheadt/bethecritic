5973e823c501e05364217c300ebc6b2e03ef2d10
mottersheadt <travis@mottershead.biz>
2012-10-06 16:46:45 -0600

    modified css globals and the default page

--- website/default.php
+++ website/default.php
@@ -7,32 +7,38 @@
         <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.min.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"
               type="text/css" media="screen" />
-        <link rel="stylesheet" href="css/add-ons.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/globals.css" type="text/css" media="screen" />
     </head>
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
-                <a class="btn btn-navbar" data-toggle="collapse"
-                   data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-                <a class="brand">Be The Critic</a>
-                <div class="nav-collapse">
-                    <ul class="nav">
-                        <li class="active"><a>Home</a></li>
-                        <li><a>How It Works</a></li>
-                        <li><a>Add Your Own</a></li>
-                        <li><a>Your Critiques</a></li>
-                    </ul>
+                <div class="container">
+                    <a href="#" class="btn btn-navbar" data-toggle="collapse"
+                       data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a href="#" class="brand">Be<b>The</b>Critic</a>
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="active"><a>Home</a></li>
+                            <li><a href="#">How It Works</a></li>
+                            <li><a href="#">Add Your Own</a></li>
+                            <li><a href="#">Your Critiques</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
             <div class="container">
                 
                 <h1>You Be The Critic!</h1>
-                <p>please.</p>
+                <p>Choose Your Topic:</p>
+                    <form class="form-search inline-form">
+                        <input type="text" class="input-medium search-query">
+                        <button type="submit" class="btn">Search</button>
+                    </form>
             </div>
         <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
     </body>
